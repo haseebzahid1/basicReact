e8d8f9bee8deb135985283f56baf5b4897b46845
haseebzahid1 <mmianhaseeb444@gmail.com>
2022-04-19 02:00:22 +0500

    react custom props card replace text  added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import ExpenseItem from './components/ExpenseItem';
 import Card from './components/card';
+import React  from "react";
+import Sdata from './components/listCard';
+console.log(Sdata[0]);
+
 function App(){
     let expenseDate = new  Date(2022, 4, 18);
     let expenseTitle = "School Fee";
@@ -11,14 +15,15 @@ function App(){
            amout={expenseAmout}>
        </ExpenseItem>
 
-       <Card 
-       imgscr="https://wallpapercave.com/wp/wp4056410.jpg"
+       {/* <Card 
+  
+       imgscr="https://images.justwatch.com/poster/8589251/s592"
        title="A Netfilx Orignal Series"
        sname="DARK"
        link="https://www.netflix.com/in/title/80990668?source=35"
        />
-       <Card 
-       imgscr="https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg"
+       <Card  
+       imgscr="https://wallpapercave.com/wp/wp4056410.jpg"
        title="A Netfilx Orignal Series"
        sname="extra curricular"
        link="https://www.netflix.com/in/title/80990668?source=35"
@@ -28,9 +33,26 @@ function App(){
        title="A Netfilx Orignal Series"
        sname="The Vampire Diaries"
        link="https://www.netflix.com/in/title/80990668?source=35"
-       />
+       /> */}
 
-        
+      <Card
+       imgscr={Sdata[0].imgscr}
+       title={Sdata[0].title}
+       sname={Sdata[0].sname}
+       link={Sdata[0].link} 
+      />
+      <Card
+       imgscr={Sdata[1].imgscr}
+       title={Sdata[1].title}
+       sname={Sdata[1].sname}
+       link={Sdata[1].link} 
+      />
+      <Card
+       imgscr={Sdata[2].imgscr}
+       title={Sdata[2].title}
+       sname={Sdata[2].sname}
+       link={Sdata[2].link} 
+      />
 
     </div>
 }
---
+++ src/components/listCard.js
@@ -0,0 +1,21 @@
+const Sdata = [
+    {
+        imgscr:"https://images.justwatch.com/poster/8589251/s592",
+        title:"A Netfilx Orignal Series",
+        sname:"DARK",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgscr:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"A Netfilx Orignal Series",
+        sname:"extra curricular",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgscr:"https://images.justwatch.com/poster/8589251/s592",
+        title:"A Netfilx Orignal Series",
+        sname:"The Vampire Diaries",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    }
+]
+export default Sdata;
